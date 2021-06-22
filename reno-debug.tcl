57b3d5900cb401abed3a83c067f470fbbea6c4b7
soheilzi <soheil.zibakhsh@gmail.com>
2021-06-22 15:03:30 +0430

    simulations and the trace files

---
+++ reno-debug.tcl
@@ -0,0 +1,116 @@
+#Create a simulator object
+set ns [new Simulator]
+
+#Define different colors for data flows (for NAM)
+$ns color 1 Blue
+$ns color 2 Red
+
+#Open the NAM trace file
+set nf [open out.nam w]
+set lg [open trace.out w]
+$ns namtrace-all $nf
+$ns trace-all $lg
+
+#Define a 'finish' procedure
+proc finish {} {
+        global ns nf
+        $ns flush-trace
+        #Close the NAM trace file
+        close $nf
+        #Execute NAM on the trace file
+        exec nam out.nam &
+        exit 0
+}
+
+#Create four nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+#Create links between the nodes
+$ns duplex-link $n0 $n2 4Mb 50ms DropTail
+$ns duplex-link $n1 $n2 4Mb 80ms DropTail
+$ns duplex-link $n2 $n3 1Mb 5ms DropTail
+$ns duplex-link $n3 $n4 4Mb 50ms DropTail
+$ns duplex-link $n3 $n5 4Mb 80ms DropTail
+
+#Set Queue Size of link (n2-n3) to 10
+$ns queue-limit $n2 $n3 10
+
+#Give node position (for NAM)
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n1 $n2 orient right-up
+$ns duplex-link-op $n2 $n3 orient right
+$ns duplex-link-op $n3 $n4 orient right-up
+$ns duplex-link-op $n3 $n5 orient right-down
+
+#Monitor the queue for link (n2-n3). (for NAM)
+$ns duplex-link-op $n2 $n3 queuePos 0.5
+
+
+#Setup a TCP connection
+set tcp [new Agent/TCP/Reno]
+$tcp set class_ 2
+$ns attach-agent $n0 $tcp
+set sink [new Agent/TCPSink]
+$ns attach-agent $n4 $sink
+$ns connect $tcp $sink
+$tcp set fid_ 1
+
+#Setup a FTP over TCP connection
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ftp set type_ FTP
+
+
+#Setup a TCP connection
+set tcp2 [new Agent/TCP/Reno]
+$tcp2 set class_ 2
+$ns attach-agent $n1 $tcp2
+set sink2 [new Agent/TCPSink]
+$ns attach-agent $n5 $sink2
+$ns connect $tcp2 $sink2
+$tcp2 set fid_ 2
+
+#Setup a FTP over TCP connection
+set ftp2 [new Application/FTP]
+$ftp2 attach-agent $tcp2
+$ftp2 set type_ FTP
+
+
+# #Setup a UDP connection
+# set udp [new Agent/UDP]
+# $ns attach-agent $n1 $udp
+# set null [new Agent/Null]
+# $ns attach-agent $n3 $null
+# $ns connect $udp $null
+# $udp set fid_ 2
+
+# #Setup a CBR over UDP connection
+# set cbr [new Application/Traffic/CBR]
+# $cbr attach-agent $udp
+# $cbr set type_ CBR
+# $cbr set packet_size_ 1000
+# $cbr set rate_ 1mb
+# $cbr set random_ false
+
+
+#Schedule events for the CBR and FTP agents
+# $ns at 0.1 "$cbr start"
+$ns at 1.0 "$ftp start"
+$ns at 1.0 "$ftp2 start"
+$ns at 400.0 "$ftp2 stop"
+$ns at 400.0 "$ftp stop"
+# $ns at 4.5 "$cbr stop"
+
+#Detach tcp and sink agents (not really necessary)
+$ns at 450 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n4 $sink"
+
+#Call the finish procedure after 5 seconds of simulation time
+$ns at 500 "finish"
+
+#Run the simulation
+$ns run
